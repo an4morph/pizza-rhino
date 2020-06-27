4744e10930f80a6621c14518a2611fb4db6cda28
an4morph <avgustin456@gmail.com>
2020-06-27 20:36:55 +0600

    styles for sign up form

--- src/components/SignupForm/Dumb.jsx
+++ src/components/SignupForm/Dumb.jsx
@@ -1,47 +1,56 @@
 import React from 'react'
 import { shape, string, func, bool } from 'prop-types'
 import Input from '../Input'
-import Button from '../Button'
+import Form from '../Form'
+import Typography from '../Typography'
+import styles from './form.module.scss'
 
 function SignupForm({
-  formName, formData, changeField, handleSubmit,
+  formName, formData, changeField, handleSubmit, className,
   loading, failed, error, showValidation, validation,
 }) {
   const { username, password, repeatPassword } = formData
 
   return (
-    <>
+    <Form
+      className={className}
+      onSubmit={handleSubmit}
+      submitting={loading}
+      buttonText="Sign up"
+      submitFailed={failed}
+      error={error}
+      title={<Typography className={styles.title} tag="h1">Sign up</Typography>}
+    >
       <Input
+        className={styles.field}
         formName={formName}
         name="username"
+        label="Username"
         value={username}
         onChange={(value) => changeField('username', value)}
         error={showValidation ? validation.username : ''}
       />
       <Input
+        className={styles.field}
         formName={formName}
         name="password"
+        label="Password"
         value={password}
         onChange={(value) => changeField('password', value)}
         type="password"
         error={showValidation ? validation.password : ''}
       />
       <Input
+        className={styles.field}
         formName={formName}
         name="repeatPassword"
+        label="Repeat password"
         value={repeatPassword}
         onChange={(value) => changeField('repeatPassword', value)}
         type="password"
         error={showValidation ? validation.repeatPassword : ''}
       />
-      {failed && <div>{error}</div>}
-      <Button
-        disabled={loading}
-        onClick={handleSubmit}
-      >
-        {loading ? 'Loading' : 'Sign up'}
-      </Button>
-    </>
+    </Form>
   )
 }
 
@@ -63,6 +72,7 @@ SignupForm.propTypes = {
   failed: bool.isRequired,
   showValidation: bool.isRequired,
   error: string,
+  className: string,
 }
 
 export default SignupForm
