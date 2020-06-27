5b4be437b5438198da7b93869e5d8b6a42ae20df
an4morph <avgustin456@gmail.com>
2020-06-27 20:31:36 +0600

    styles for  profile changes form

--- src/components/ProfileChangesForm/Dumb.jsx
+++ src/components/ProfileChangesForm/Dumb.jsx
@@ -1,36 +1,47 @@
 import React from 'react'
 import { shape, string, func, bool } from 'prop-types'
 import Input from '../Input'
-import Button from '../Button'
+import Form from '../Form'
+import Typography from '../Typography'
+import styles from './form.module.scss'
 
 function ProfileChangesForm({
-  formName, formData, changeField, handleSubmit,
-  showValidation, validation,
+  formName, formData, changeField, handleSubmit, failed,
+  showValidation, validation, className, loading, error,
 }) {
   const { password, repeatPassword } = formData
 
   return (
-    <>
+    <Form
+      className={className}
+      onSubmit={handleSubmit}
+      submitting={loading}
+      buttonText="Log in"
+      submitFailed={failed}
+      error={error}
+      title={<Typography className={styles.title} tag="h2">Change password</Typography>}
+    >
       <Input
+        className={styles.field}
         formName={formName}
         name="password"
+        label="Password"
         value={password}
         onChange={(value) => changeField('password', value)}
         type="password"
         error={showValidation ? validation.password : ''}
       />
       <Input
+        className={styles.field}
         formName={formName}
         name="repeatPassword"
+        label="Repeat password"
         value={repeatPassword}
         onChange={(value) => changeField('repeatPassword', value)}
         type="password"
         error={showValidation ? validation.repeatPassword : ''}
       />
-      <Button onClick={handleSubmit}>
-        Confirm changes
-      </Button>
-    </>
+    </Form>
   )
 }
 
@@ -47,6 +58,10 @@ ProfileChangesForm.propTypes = {
   handleSubmit: func.isRequired,
   formName: string.isRequired,
   showValidation: bool.isRequired,
+  className: string,
+  loading: bool.isRequired,
+  failed: bool.isRequired,
+  error: string,
 }
 
 export default ProfileChangesForm
