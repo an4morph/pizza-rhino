8eeec80a5a1190d0c029e4a6a463a2ed37183cfc
an4morph <avgustin456@gmail.com>
2020-06-27 19:15:33 +0600

    add styles for input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,22 +1,25 @@
 import React from 'react'
+import cx from 'classnames'
 import { string, func, node } from 'prop-types'
+import styles from './input.module.scss'
 
 function Input({
-  label, formName, name, value, onChange, error = '', ...rest
+  label, formName, name, value, onChange, error = '', className, ...rest
 }) {
   const id = `${formName}-${name}`
 
   return (
-    <label htmlFor={id}>
-      {label && <span>{label}</span>}
+    <label htmlFor={id} className={cx(styles.container, className)}>
+      {label && <span className={styles.label}>{label}</span>}
       <input
+        className={cx(styles.input, { [styles['input-error']]: error })}
         id={id}
         value={value}
         name={name}
         onChange={(e) => onChange(e.target.value)}
         {...rest}
       />
-      {error && <span>{error}</span>}
+      {error && <span className={styles.error}>{error}</span>}
     </label>
   )
 }
@@ -27,6 +30,7 @@ Input.propTypes = {
   name: string.isRequired,
   formName: string.isRequired,
   value: string,
+  className: string,
   onChange: func.isRequired,
 }
 
