bc0937f509320975dc9f512461bf2b43d42c6764
Polina Antonova <anamorph@MacBook-Pro-Polina.local>
2020-07-30 18:17:00 +0600

    layout for auth block in sidebar

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,24 +1,29 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import { func, node, string } from 'prop-types'
 import cx from 'classnames'
 import styles from './button.module.scss'
 
-function Button({ children, onClick, className }) {
-  return (
+function Button({ link, children, onClick, className }) {
+  const getButton = (clsName) => (
     <button
       type="button"
       onClick={onClick}
-      className={cx(styles.button, className)}
+      className={clsName}
     >
       {children}
     </button>
   )
+  return link
+    ? <NavLink className={cx(styles.link, className)} to={link}>{getButton(styles.button)}</NavLink>
+    : getButton(cx(styles.button, className))
 }
 
 Button.propTypes = {
   onClick: func,
   children: node.isRequired,
   className: string,
+  link: string,
 }
 
 export default Button
--- src/components/Sidebar/AuthBlock.js
+++ src/components/Sidebar/AuthBlock.js
@@ -1,14 +1,37 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import Button from '../Button'
+import IconButton from '../IconButton'
+import LogoutIcon from '../Icons/Logout'
+import UserIcon from '../Icons/User'
 import styles from './sidebar.module.scss'
+
+const logged = false
 
 function AuthBlock() {
   return (
-    <div>
-      AuthBlock
+    <div className={styles.auth}>
+      {
+        logged
+          ? (
+            <NavLink to="/profile" className={styles.auth__user}>
+              <UserIcon />
+              <div className={styles.username}>username</div>
+            </NavLink>
+          )
+          : <div className={styles.auth__text}>Login to receive bonuses and gifts</div>
+      }
+      {
+        logged
+          ? <IconButton icon={<LogoutIcon />} onClick={() => console.log('logout')} />
+          : <Button className={styles.auth__login} link="/login">Login</Button>
+      }
     </div>
   )
 }
 
-AuthBlock.propTypes = {}
+AuthBlock.propTypes = {
+
+}
 
 export default AuthBlock
